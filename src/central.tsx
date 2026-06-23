6773f54f5ec54924fda21bd7db9a959997afc9a9
Juan Ignacio Bayúgar <juanibayugar@gmail.com>
2026-06-23 20:20:44 -0300

    8

--- src/central.tsx
+++ src/central.tsx
@@ -1,8 +1,8 @@
-import img1 from "./assets/imagenes/cuestionario/no lo se d-e/no lo se 3.png";
-import img2 from "./assets/imagenes/cuestionario/no lo se d-e/no lo se 2.png";
-import img3 from "./assets/imagenes/cuestionario/no lo se d-e/no lo se 1.png";
-import img4 from "./assets/imagenes/cuestionario/no lo se d-e/no lo se 4.png";
-import img6 from "./assets/imagenes/cuestionario/no lo se d-e/no lo se 6.png";
+import img1 from "/assets/imagenes/cuestionario/no lo se d-e/no lo se 3.png";
+import img2 from "/assets/imagenes/cuestionario/no lo se d-e/no lo se 2.png";
+import img3 from "/assets/imagenes/cuestionario/no lo se d-e/no lo se 1.png";
+import img4 from "/assets/imagenes/cuestionario/no lo se d-e/no lo se 4.png";
+import img6 from "/assets/imagenes/cuestionario/no lo se d-e/no lo se 6.png";
 import { RutaProtegida } from "./components/RutaProtegida.tsx"; // El guardia que creamos recién
 
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
